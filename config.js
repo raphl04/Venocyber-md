81a6dc9f6082dfe4e939348292355f43aa64b189
raphl04 <141498363+raphl04@users.noreply.github.com>
2024-05-16 10:00:55 +0100

    Update config.mdf

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
 global.email = "losrios755@gmail.com";
 global.location = "Njombe,Tanzania.";
-global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
@@ -20,7 +19,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://te
 global.devs = "255625774543";
 global.sudo = process.env.SUDO || "255625774543";
 global.owner = process.env.OWNER_NUMBER || "255625774543";
-global.style = process.env.STYLE || "3";
+global.style = process.env.STYLE || "2";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
 global.warncount = process.env.WARN_COUNT || 3;
@@ -33,13 +32,13 @@ global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
 global.read_status = process.env.AUTO_READ_STATUS || "true";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "true";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
-
+global.anti_delete = process.env.ANTI_DELETE || "true";
 global.api_smd = "https://api-smd-1.vercel.app";
 global.scan = "https://rb.gy/nl69nj";
-
+global.auto_react = process.env.AUTO_REACT || "true" 
 global.SESSION_ID =
   process.env.SESSION_ID ||
   ""
